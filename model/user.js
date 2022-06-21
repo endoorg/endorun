15cb8c518e9bcd1372d794126baf947f76266ed8
Apor Bokor <aporbokor@protonmail.com>
2022-06-21 18:20:10 +0200

    major changes regarding the backend

--- model/user.js
+++ model/user.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose')
 
 const UserSchema = new mongoose.Schema({
 	username: { type: String, required: true, unique: true },
-	password: { type: String, required: true }
+	password: { type: String, required: true },
+	email: { type: String, required: true },
+	hour: { type: Number, required: true },
+	minute: { type: Number, required: true },
+	distance: { type: Number, required: true },
 }, { collection: 'users' })
 
 const model = mongoose.model('UserSchema', UserSchema)
--- server.js
+++ server.js
@@ -1,72 +1,125 @@
-const express = require('express')
-const path = require('path')
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
-const User = require('./model/user')
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
+require('dotenv').config();
 
-const JWT_SECRET = 'atuheont1561HTNOEHUNthonuhork0615@@AUOhNTOHUJ  OTHUN){[( onuhej*&#(jt'
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const User = require('./model/user');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const JWT_SECRET = process.env.JWT_SECRET;
 
 mongoose.connect('mongodb://localhost:27017/endodb', {
-	useNewUrlParser: true,
-	useUnifiedTopology: true
-})
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-const app = express()
-app.use('/', express.static(path.join(__dirname, 'static')))
-app.use(bodyParser.json())
+const app = express();
+
+app.use('/', express.static(path.join(__dirname, 'views')));
+app.use(bodyParser.json());
+
+app.set('view-engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index.ejs');
+});
+
+app.get('/result', (req, res) => {
+  res.render('result.ejs', { name: 'Api' });
+});
+
+app.get('/login', (req, res) => {
+  res.render('login.ejs');
+});
+
+app.post('/api/result', async (req, res) => {
+  const { token } = req.body;
+  console.log(token);
+  try {
+    const user = jwt.verify(token, JWT_SECRET);
+    console.log('server: ', user);
+    console.log(user.username);
+    const username = user.username;
+    const data = await User.findOne({ username }).lean();
+    console.log(data);
+    return res.json({
+      status: 'ok',
+      username: data.username,
+      hour: data.hour,
+      minute: data.minute,
+      distance: data.distance,
+    });
+  } catch (err) {
+    return res.json({ status: 'error', error: err });
+  }
+});
 
 app.post('/api/login', async (req, res) => {
-	const { username, password } = req.body 
+  const { username, password } = req.body;
 
-	const user = await User.findOne({ username }).lean()
+  if (!username || typeof username != 'string') {
+    return res.json({ status: 'error', error: 'invalid username' });
+  }
 
-	if (!user) {
-		return res.json({ status: 'error', error: 'invalid username/password' })
-	}
-	if (await bcrypt.compare(password, user.password)) {
+  if (!password || typeof password != 'string') {
+    return res.json({ status: 'error', error: 'invalid password' });
+  }
 
-		const token = jwt.sign({ id: user._id, username: user.username }, JWT_SECRET)
-		return res.json({ status: 'ok', data: token })
-	}
+  const user = await User.findOne({ username }).lean();
 
-	res.json({ status: 'error', error: 'invalid username/password' })
-})
+  if (!user) {
+    return res.json({ status: 'error', error: 'invalid username/password' });
+  }
+  if (await bcrypt.compare(password, user.password)) {
+    const token = jwt.sign(
+      { id: user._id, username: user.username },
+      JWT_SECRET
+    );
+    return res.json({ status: 'ok', data: token });
+  }
+  res.json({ status: 'error', error: 'invalid username/password' });
+});
 
-app.post('/api/register', async (req, res)=> {
-	const { username, password: plaintext } = req.body
+app.post('/api/register', async (req, res) => {
+  const {
+    username,
+    email,
+    password: plaintext,
+    hour,
+    minute,
+    distance,
+  } = req.body;
 
-	if (!username || typeof(username) != 'string') {
-		return res.json({ status: 'error', error: 'invalid username' })
-	}
+  if (!username || typeof username != 'string') {
+    return res.json({ status: 'error', error: 'invalid username' });
+  }
 
-	if (!plaintext || typeof(plaintext) != 'string') {
-		return res.json({ status: 'error', error: 'invalid password' })
-	}
-	const password = await bcrypt.hash(plaintext, 15)
+  if (!plaintext || typeof plaintext != 'string') {
+    return res.json({ status: 'error', error: 'invalid password' });
+  }
+  const password = await bcrypt.hash(plaintext, 10);
 
-
-	try {
-		const response = await User.create({
-			username, password
-		})
-		console.log('user created successfully: ', response)
-
-	} catch(error) {
-		if (error.code === 11000) {
-			return res.json({ status: 'error', error: 'username already in use' })
-		}
-		throw error
-	}
-
-	res.json({ status: 'ok' })
-
-})
-
-
+  try {
+    const response = await User.create({
+      username,
+      password,
+      email,
+      hour,
+      minute,
+      distance,
+    });
+    console.log('user created successfully: ', response);
+  } catch (error) {
+    if (error.code === 11000) {
+      return res.json({ status: 'error', error: 'username already in use' });
+    }
+    throw error;
+  }
+  res.json({ status: 'ok' });
+});
 
 app.listen(3000, () => {
-	console.log('server up at 3000')
-})
-
+  console.log('server up at 3000');
+});
